e21de9d5db7df650acd18b2cee00ed7d4529277f
fhl96 <965424025@qq.com>
2022-09-15 11:41:17 +0800

    lodash

--- lodash/fhl965-lodash.js
+++ lodash/fhl965-lodash.js
@@ -27,9 +27,39 @@ var fhl965 = function() {
     return result
   }
 
+  function difference(...ary2) {
+    result = []
+    var a = []
+    for (var i = 1;i < arguments.length;i++){
+        a = a.concat(arguments[i])
+    }
+    var b = {}
+    var c = {}
+    for (var i = 0;i < arguments[0].length;i++) {
+      if (!(arguments[0][i] in b)) {
+        b[arguments[0][i]] = 1
+      }
+      b[arguments[0][i]]++
+    }
+    for (var i = 0;i < a.length;i++) {
+      if (!(a[i] in c)) {
+        c[a[i]] = 1
+      }
+      c[a[i]]++
+    }
+    for (var key in b) {
+      if (!(key in c)) {
+          var nkey = Number(key)
+        result.push(nkey)
+      }
+    }
+    return result
+  }
+
 return {
   chunk,
   compact,
+  difference,
 }
 
 
