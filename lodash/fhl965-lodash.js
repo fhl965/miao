af87a885e587fcf85d97d5ecc21d1b95aa5f8d74
fhl96 <965424025@qq.com>
2022-09-26 22:00:15 +0800

    lodash

--- lodash/fhl965-lodash.js
+++ lodash/fhl965-lodash.js
@@ -56,10 +56,113 @@ var fhl965 = function() {
     return result
   }
 
+  function drop(ary,val = 1) {
+    var result = []
+    for (var i = val;i < ary.length;i++) {
+      result.push(ary[i])
+    }
+    return result
+  }
+
+  function dropRight(ary,val = 1) {
+    var result = []
+    for (var i = 0;i < ary.length - val;i++) {
+      result.push(ary[i])
+    }
+    return result
+  }
+
+  function dropRightWhile(ary,val) {
+
+  }
+  function fill(ary,val,start = 0,end = ary.length) {
+    for (var i = start;i < end;i++) {
+      ary[i] = val
+    }
+    return ary
+  }
+  function findIndex(ary,predicate,fromIndex = 0) {
+    if (typeof predicate == 'function') {
+      for (var i = fromIndex;i < ary.length;i++) {
+        var item = ary[i]
+        if(predicate(item)) {
+          return i
+        }
+      }
+      return -1
+    }
+    if (Array.isArray(predicate)) {
+      for (var i = fromIndex;i < ary.length;i++) {
+        if(ary[i][predicate[0]] == predicate[1]) {
+          return i
+        }
+      }
+      return -1
+    }
+    if (typeof predicate == 'object') {
+      for (var i = fromIndex;i < ary.length;i++) {
+        var item = ary[i]
+        var count = 1
+        for (var key in item) {
+          if (!(key in predicate) || item[key] != predicate[key] ) {
+            count = 0
+          }
+        }
+        if (count == 1) {
+          return i
+        }
+      }
+      return -1
+    }
+    if (typeof predicate == 'string') {
+      for (var i = fromIndex;i < ary.length;i++) {
+        var item = ary[i]
+        if(ary[i][predicate] == true) {
+          return i
+        }
+      }
+      return -1
+    }
+  }
+  function flatten(array) {
+    var result = []
+    for (var i = 0;i < array.length;i++) {
+      if (!Array.isArray(array[i])) {
+        result.push(array[i])
+      } else {
+        for (var j = 0;j < array[i].length;j++) {
+          result.push(array[i][j])
+        }
+      }
+    }
+    return result
+  }
+  function flattenDeep(array) {
+    var result = []
+    function inFlatten(array) {
+      for (var i = 0;i < array.length;i++) {
+        if (!Array.isArray(array[i])) {
+          result.push(array[i])
+        } else {
+          inFlatten(array[i])
+        }
+      }
+    }
+    inFlatten(array)
+    return result
+  }
+
+
+
 return {
   chunk,
   compact,
   difference,
+  drop,
+  dropRight,
+  fill,
+  findIndex,
+  flatten,
 }
 
 
